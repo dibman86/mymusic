34b2ef8437250403fe2380f1d95b97411be8c0a8
dibman86 <i.ennajih@gmail.com>
2025-04-01 07:15:21 +0000

    Update carousel3dyt.js

--- js/carousel3dyt.js
+++ js/carousel3dyt.js
@@ -139,16 +139,13 @@ function Carousel3Dspirale() {
 		}
 	}
 
-	(function loadkey(){
+	function loadVids(playlistId, token, videoid) {
+		if (!token) token = '';
 		const YT_API_KEY = document.createElement('script');
 		YT_API_KEY.src = "./config.js"; // Chargé par GitHub Actions
 		document.head.appendChild(YT_API_KEY);
-	})();
-
-	function loadVids(playlistId, token, videoid) {
-		if (!token) token = '';
 		YT_API_KEY.onload = function() {
-			if(!API_KEY) loadkey();
+			if(!API_KEY) loadVids();
 			console.log(API_KEY)
 			const key = API_KEY;
 			const url = 'https://www.googleapis.com/youtube/v3/playlistItems';
